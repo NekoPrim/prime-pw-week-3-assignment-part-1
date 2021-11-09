410da54382dc0be9b5868e79de0a03b3cef74898
Desiree Quade <nekodez@gmail.com>
2021-11-09 14:48:42 -0600

    Question 4

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -9,9 +9,9 @@ console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
 let name='Desiree';
-let favoriteFoods=['raspberries',' strawberries',' pizza',' cheesecake'];
+let favoriteFoods=['raspberries','strawberries','pizza','cheesecake'];
 
-console.log(name+'`s favorite foods are '+favoriteFoods);
+console.log(name+'`s favorite foods are:'+favoriteFoods);
 // 1.b. TODO: Log your array of foods to the console with a message, similar
 //      to the example above
 
@@ -55,7 +55,8 @@ console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
 
-
+favoriteFoods.push('blueberries');
+console.log('Added a favorite food to end, ',favoriteFoods);
 
 // Example: Remove the last animal by using Array.pop
 let removedAnimal = animalArray.pop();
@@ -65,6 +66,9 @@ console.log('The animals are now', animalArray);
 // 4.b. TODO: Remove the food at the end of your array &
 //      log both the food removed and the updated array
 
+let removedFood=favoriteFoods.pop();
+console.log('Removed the last favorite food',removedFood);
+console.log('My favorite foods are now',favoriteFoods);
 
 // Example: Add an animal to the beginning using Array.unshift
 animalArray.unshift('walrus');
@@ -72,6 +76,8 @@ console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
 
+favoriteFoods.unshift('mangos');
+console.log(`Added a favorite food to beginning: ${favoriteFoods}`);
 
 // Example: Remove the first animal using Array.shift
 removedAnimal = animalArray.shift();
@@ -80,3 +86,7 @@ console.log('The animals are now', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array &
 //     log both the food removed and the updated array
+
+removedFood=favoriteFoods.shift();
+console.log('Removed the first favorite food',removedFood);
+console.log('My favorite foods are now',favoriteFoods);
